2fdacc40cad010593f066086192fa4f59d777eb8
Samuelwsz <Samuelxwsz@gmail.com>
2023-06-03 18:48:50 -0300

    att

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,46 @@
 import Cabecalho from "@/Components/Cabecalho"
 import styles from "./Home.module.css"
 import Card from "@/Components/Card"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [plantas, setPlantas] = useState([])
+  const [filtroPreco, setFiltroPreco] = useState("")
+  const [filtroNome, setFiltroNome] = useState("")
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/samuelwsz/api7DaysOfCodeReact/plantas"
+    )
+      .then((reposta) => reposta.json())
+      .then((dados) => setPlantas(dados))
+  }, [])
+
+  const handleFiltrarPreco = () => {
+    const filteredPlantas = plantas.filter(
+      (planta) => planta.preco <= filtroPreco
+    )
+    setPlantas(filteredPlantas)
+  }
+
+  const handleFiltrarNome = () => {
+    const filteredPlantas = plantas.filter((planta) =>
+      planta.nome.toLowerCase().includes(filtroNome.toLowerCase())
+    )
+    setPlantas(filteredPlantas)
+  }
+
+  const handleLimparFiltro = () => {
+    setFiltroPreco("")
+    setFiltroNome("")
+    // Recarrega a lista original de plantas da API
+    fetch(
+      "https://my-json-server.typicode.com/samuelwsz/api7DaysOfCodeReact/plantas"
+    )
+      .then((reposta) => reposta.json())
+      .then((dados) => setPlantas(dados))
+  }
+
   return (
     <>
       <Cabecalho />
@@ -10,13 +48,36 @@ export default function Home() {
         <div className={styles.home}>
           <p>Conheça nossas</p>
           <p>ofertas</p>
+          <div>
+            <input
+              type="number"
+              placeholder="Filtrar por preço"
+              value={filtroPreco}
+              onChange={(e) => setFiltroPreco(e.target.value)}
+            />
+            <button onClick={handleFiltrarPreco}>Filtrar</button>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Filtrar por nome"
+              value={filtroNome}
+              onChange={(e) => setFiltroNome(e.target.value)}
+            />
+            <button onClick={handleFiltrarNome}>Filtrar</button>
+          </div>
+          <button onClick={handleLimparFiltro}>Limpar Filtro</button>
         </div>
+
         <section>
-          <Card
-            imagem="/Images/Ajuga_reptans.png"
-            nome="Ajuga reptans"
-            preco="50,00"
-          />
+          {plantas.map((planta) => {
+            return (
+              <Card
+                {...planta}
+                key={planta.id}
+              />
+            )
+          })}
         </section>
       </div>
     </>
